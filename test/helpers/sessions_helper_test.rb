dc335d315945c9b3dacf36d657cf07f6ebd55ed2
echo8810 <watanabe-b.p8810@ezweb.ne.jp>
2019-04-02 06:28:54 +0900

    init commit

---
+++ test/helpers/sessions_helper_test.rb
@@ -0,0 +1,26 @@
+=begin
+1.fixtureでuser変数を定義する
+2.渡されたユーザーをrememberメソッドで記憶する
+3.current_userが、渡されたユーザーと同じであることを確認します
+=end
+
+
+require 'test_helper'
+
+class SessionsHelperTest < ActionView::TestCase
+
+  def setup
+    @user = users(:michael)
+    remember(@user)
+  end
+
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
+  end
+
+  test "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
+  end
+end
